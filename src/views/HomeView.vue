9fcd8487b57fcc8534079756743f4d3d74b0368f
Aegidius <Aegidius.Haslauer@Student.HTW-Berlin.de>
2023-05-31 17:43:04 +0200

    changes to home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,74 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class = "background">
+    <div class = "left-side"></div>
+    <div class = "right-side"></div>
+  </div>
+
+  <div class = "content">
+    <div class = "left-side">
+      <div class = "settings-button" alt = "settings button">
+        <img class = "settings-icon" alt = "settings icon" src = "../assets/settings.png">
+        <div class = "button-text">Settings</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
+
+<style>
+  body {
+    margin: 0;
+    padding: 0;
+    position: relative;
+  }
+  .background {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+  .left-side {
+    background-color: #20c9c1;
+    width: 15%;
+    height: 100vh;
+    float: left;
+  }
+  .right-side {
+    background-color: #f8efef;
+    width: 85%;
+    height: 100vh;
+    float: left;
+  }
+  .content {
+    z-index: 1;
+  }
+  .settings-button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 50px;
+  }
+  .settings-icon {
+    position: relative;
+    display: flex;
+    margin-right: 5px;
+    /* left: 2.2%; */
+    width: 40px;
+    height: 40px;
+    z-index: 1;
+  }
+  .button-text {
+    position: relative;
+    margin-left: 10px;
+    font-weight: bold;
+  }
+</style>
