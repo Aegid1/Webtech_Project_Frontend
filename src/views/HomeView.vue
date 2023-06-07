377791100ee546426ec4729c433cc072d0db165a
ProgMojo <m.moussa2422@gmail.com>
2023-06-07 17:05:05 +0200

    "HTML ergänzt - HomeView"

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <head>
+
     <link rel= "stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
   </head>
