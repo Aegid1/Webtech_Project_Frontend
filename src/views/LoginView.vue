11c1ab20f0702120b359210977e15b6ab3d8f8eb
ProgMojo <m.moussa2422@gmail.com>
2023-06-20 17:49:18 +0200

    "wow"

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -8,21 +8,23 @@
       <div class="col-md-6">
          <form>
         <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Email address</label>
+          <label for="exampleInputEmail1" class="form-label">Email address </label>
           <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
           <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+          <br>
         </div>
         <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Password</label>
+          <label for="exampleInputPassword1" class="form-label">Password </label>
           <input type="password" class="form-control" id="exampleInputPassword1">
         </div>
+           <br>
         <div class="mb-3 form-check">
           <input type="checkbox" class="form-check-input" id="exampleCheck1">
           <label class="form-check-label" for="exampleCheck1">Check me out</label>
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
       </form></div>
-      <div class="col-md-3"></div>
+      <div class="col-md-9"></div>
     </div>
   </div>
 </template>
@@ -34,5 +36,12 @@ export default {
 </script>
 
 <style scoped>
-
+.col-md-3 {
+  display: grid;
+  grid-template-rows: 40% 20%;
+  height: 100vh;
+  width: 100vh;
+  color: aqua;
+  background-color: aqua;
+}
 </style>
