e2bd9cd17abb6f5f97dba4795b4c68be8e2351e2
ProgMojo <m.moussa2422@gmail.com>
2023-06-21 13:59:17 +0200

    Login-Form verbesserung erste Schritte

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -6,24 +6,23 @@
     <div class="row">
       <div class="col-md-3"></div>
       <div class="col-md-6">
-         <form>
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Email address </label>
+        <form>
+        <div class="loginForm">
+          <label for="exampleInputEmail1" class="form-label"><b>Email address </b> </label>
           <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-          <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
           <br>
+          <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
         </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Password </label>
+        <div class="loginForm">
+          <label for="exampleInputPassword1" class="form-label"><b>Password </b></label>
           <input type="password" class="form-control" id="exampleInputPassword1">
         </div>
            <br>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input"
-                 id="exampleCheck1">
+        <div class="loginForm form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1">
           <label class="form-check-label" for="exampleCheck1">Check me out</label>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="Login" class="btn btn-primary">Login</button>
       </form></div>
       <div class="col-md-9"></div>
     </div>
@@ -37,12 +36,21 @@ export default {
 </script>
 
 <style scoped>
-.col-md-3  {
+.col-md-3 {
   display: grid;
   grid-template-rows: 40% 20%;
   height: 100vh;
-  width: 100vh;
-  color: aqua;
-  background-color: aqua;
+  width: 20%;
+  z-index: 0;
+  border-radius: 0px 30px 0px 0px;
+  background-color: #20c9c1;
+}
+
+.loginForm {
+  background-color: #20c9c1;
+  margin: auto;
+  max-width: 350px;
+  padding: 10px;
+  text-transform:
 }
 </style>
