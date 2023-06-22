34b19be045c86e14d0ff08e6c57f205f9d994caf
ProgMojo <m.moussa2422@gmail.com>
2023-06-22 12:57:37 +0200

    Fehlerbehebung der Horizontal angereihten Divs

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,23 +4,26 @@
       <div class="firstDiv"></div>
       <div class="secondDiv">
         <form>
-          <div class="loginForm">
-            <label for="exampleInputEmail1" class="Form-titles"><b>Email address </b> </label>
-            <input type="email" class="InputFields" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-          </div>
-          <div class="loginForm">
-            <br>
-            <label for="exampleInputPassword1" class="Form-titles"><b>Password </b></label>
-            <input type="password" class="InputFields" id="exampleInputPassword1">
-          </div>
-          <div class="loginForm form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-          </div>
+          <div class ="WrapperLoginForm">
+            <div class="loginForm">
+              <label for="exampleInputEmail1" class="Form-titles"><b>Email address </b> </label>
+              <input type="email" class="InputFields" id="exampleInputEmail1" aria-describedby="emailHelp">
+              <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+              <div class="loginForm">
+              <br>
+              <label for="exampleInputPassword1" class="Form-titles"><b>Password </b></label>
+              <input type="password" class="InputFields" id="exampleInputPassword1">
+            </div>
+            <div class="loginForm form-check">
+              <input type="checkbox" class="form-check-input" id="exampleCheck1">
+              <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
           <br>
-          <button type="submit" class="LoginButton">Login</button>
-        </form></div>
+          <button type="submit" class="LoginButton"><b>Login</b></button>
+          </div>
+        </form>
+      </div>
       <div class="thirdDiv"></div>
     </div>
   </div>
@@ -44,7 +47,7 @@ export default {
 }
 
 .secondDiv{
-  background-color: white;
+  background-color: red;
   width: 60%;
   height: 0px;
   z-index: 0;
@@ -60,19 +63,34 @@ export default {
   float: right;
 }
 
-.loginForm {
+.WrapperLoginForm {
   background-color: #20c9c1;
-}
-
-.LoginButton{
-  background-color: White;
-  max-width: 350px;
-  padding: 5px;
+  padding: 20px;
+  border-radius: 30px 30px 30px 30px;
+  height: 70vh;
 }
 
 .InputFields{
   display: inline-block;
   width: 80%;
+}
+
+.form-text{
+  margin: 10px;
+}
+
+.form-check-input{
+  margin: 15px;
+}
+
+.LoginButton{
+  background-color: White;
+  max-width: 350px;
+  padding: 15px;
+  padding-left: 25px;
+  padding-right: 25px;
+  margin: 0px;
+  border-radius: 15px 15px 15px 15px;
 }
 
 /*.Form-titles{
